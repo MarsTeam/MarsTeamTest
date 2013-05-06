b005ffbefa7a1c0d6e650cde577b6d85244c4f09
zhang1989sh <zhang1989sh@gmail.com>
2013-05-06 09:47:41 +0800

    PHP 服务器 图片上传测试成功

--- TEST2/TEST2/ViewController.m
+++ TEST2/TEST2/ViewController.m
@@ -24,12 +24,9 @@
     NSString *contents = @"您有一条未读的新消息";
     [self localNotification:contents];
     
-    //NSString *aurl = @"http://localhost/host/upload_file.php";
-    //[self loadImage:aurl];
+    UIImage *image = [UIImage imageNamed:@"mars.jpg"];
+    [self imageUpload:image];
     
-    [self uploadImage];
-    
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
 
@@ -72,140 +69,36 @@
     
 }
 
-
--(void)loadImage:(NSString*)aurl
-{
-    NSData              *imageData;
-    NSMutableData       *postBody;
-    NSString            *stringBoundary, *contentType;
-    NSURL *url = [NSURL URLWithString:aurl];  //将字符串转换为NSURL格式
-    
-    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    NSString *documentsDirectory = [paths objectAtIndex:0];
-    NSString *path = [documentsDirectory stringByAppendingPathComponent:@"mars.jpeg"];
-    
-    imageData = [[NSData alloc] initWithContentsOfFile:path];
-    
-    ASIFormDataRequest *aRequest = [[ASIFormDataRequest alloc] initWithURL:url];
-    
-    //stringBoundary = [NSString stringWithString:@"0xKhTmLbOuNdArY"];
-    stringBoundary = @"0xKhTmLbOuNdArY";
-    
-    contentType    = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", stringBoundary];
-    
-    [aRequest addRequestHeader:@"Content-Type" value:contentType];
-    
-    postBody = [[NSMutableData alloc] init];
-    
-    [postBody appendData:[[NSString stringWithFormat:@"\r\n\r\n--%@\r\n", stringBoundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    
-//    [postBody appendData:[[NSString stringWithString:@"Content-Disposition: form-data; name=\"title\"\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    [postBody appendData:[@"Content-Disposition: form-data; name=\"title\"\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-    
-//    [postBody appendData:[[NSString stringWithString:@"lighttable"] dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    [postBody appendData:[@"lighttable" dataUsingEncoding:NSUTF8StringEncoding]];
-    // So Light Table show up as source in Twitter post
-    
-    NSString *imageFileName = [NSString stringWithFormat:@"mars.jpeg"];
-    
-    [postBody appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", stringBoundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    [postBody appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"upload\"; filename=\"%@\"\r\n",imageFileName] dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    [postBody appendData:[@"Content-Type: image/jpeg\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    [postBody appendData:imageData];
-    
-    [postBody appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", stringBoundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    
-//    [postBody appendData:[[NSString stringWithString:@"Content-Disposition: form-data; name=\"password\"\r\n\r\n"] dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    [postBody appendData:[@"Content-Disposition: form-data; name=\"password\"\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    
-    [postBody appendData:[@"lighttablexxxxxxxx" dataUsingEncoding:NSUTF8StringEncoding]];
-    
-//    [postBody appendData:[[NSString stringWithString:@"lighttablexxxxxxxx"] dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    [postBody appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n", stringBoundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    [aRequest setDelegate:self];
-    
-    [aRequest appendPostData:postBody];
-    
-    [aRequest setRequestMethod:@"POST"];
-    
-    [aRequest startAsynchronous];
-}
-
-
-- (IBAction)uploadImage {
-    /*
-     turning the image into a NSData object
-     getting the image back out of the UIImageView
-     setting the quality to 90
-     */
-//    UIImage *myImage = [UIImage imageNamed:@"mars.jpg"];
-//    
-//    NSData *imageData = UIImageJPEGRepresentation(myImage, 1.0);
-    
-    
-    //UIImage *m_imgFore=[UIImage imageNamed:@"mars.jpg"];;
-    //png格式
-    //NSData *imageData=UIImagePNGRepresentation(m_imgFore);
-    //JEPG格式
-    //NSData *imageData=UIImageJPEGRepresentation(m_imgFore,1.0);
-    
-    UIImage *image = [UIImage imageNamed:@"mars.jpg"];
-    
-    NSData *imageData= UIImageJPEGRepresentation(image, 0.5);
-    
-    
-//    UIImage *img = [UIImage imageNamed:@"some.png"];
-//    NSData *dataObj = UIImageJPEGRepresentation(img, 1.0);
-    
-    
-    // setting up the URL to post to
-    //NSString *urlString = @"http://www.ccbase.net/test-upload.php";
+//上传图片方法
+- (void) imageUpload:(UIImage *) image{
+    //把图片转换成imageDate格式
+    NSData *imageData = UIImageJPEGRepresentation(image, 1.0);
+    //传送路径
     NSString *urlString = @"http://localhost/host/upload_file.php";
-    
-    // setting up the request object now
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    //建立请求对象
+    NSMutableURLRequest * request = [[NSMutableURLRequest alloc] init];
+    //设置请求路径
     [request setURL:[NSURL URLWithString:urlString]];
+    //请求方式
     [request setHTTPMethod:@"POST"];
-    
-    /*
-     add some header info now
-     we always need a boundary when we post a file
-     also we need to set the content type
-     
-     You might want to generate a random boundary.. this is just the same
-     as my output from wireshark on a valid html post
-     */
+    //一连串上传头标签
     NSString *boundary = @"---------------------------14737809831466499882746641449";
     NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
     [request addValue:contentType forHTTPHeaderField: @"Content-Type"];
-    
-    /*
-     now lets create the body of the post
-     */
     NSMutableData *body = [NSMutableData data];
-    [body appendData:[[NSString stringWithFormat:@"rn--%@rn",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:[@"Content-Disposition: form-data; name=\"userfile\"; filename=\"ipodfile.jpg\"rn" dataUsingEncoding:NSUTF8StringEncoding]];
-    [body appendData:[@"Content-Type: application/octet-streamrnrn" dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",boundary]dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[@"Content-Disposition: form-data; name=\"userfile\"; filename=\"vim_go.jpg\"\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+    [body appendData:[@"Content-Type: application/octet-stream\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
     [body appendData:[NSData dataWithData:imageData]];
-    [body appendData:[[NSString stringWithFormat:@"rn--%@--rn",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
-    // setting the body of the post to the reqeust
+    [body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",boundary]dataUsingEncoding:NSUTF8StringEncoding]];
     [request setHTTPBody:body];
-    
-    // now lets make the connection to the web
+    //上传文件开始
     NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    //获得返回值
     NSString *returnString = [[NSString alloc] initWithData:returnData encoding:NSUTF8StringEncoding];
-    
-    NSLog(@"returnString:%@",returnString);
+    NSLog(@"%@",returnString);
 }
+
 
 
 - (void)didReceiveMemoryWarning
