d8eb0c4665e1b49fdd4522475525bd6094fccc3e
mars1989sh <zhangweichao1989@gmail.com>
2013-06-01 14:07:03 +0800

    老虎机demo OK～

--- TEST2/TEST2/ViewController.m
+++ TEST2/TEST2/ViewController.m
@@ -12,7 +12,13 @@
 
 
 @interface ViewController ()
-
+{
+    int j;
+    NSTimer *times;
+    NSArray *column1;
+    NSArray *column2;
+    NSArray *column3;
+}
 @end
 
 @implementation ViewController
@@ -20,6 +26,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
     NSLog(@"羊你改成你的名字");
     NSString *contents = @"您有一条未读的新消息";
     [self localNotification:contents];
@@ -27,6 +34,37 @@
     UIImage *image = [UIImage imageNamed:@"mars.jpg"];
     [self imageUpload:image];
     
+    
+    j = 0;
+    CGRect pickerFrame = CGRectMake(0, 0, 260, 80);
+    self.picker.frame = pickerFrame;
+    
+    UIImage *happy=[UIImage imageNamed:@"happy.png"];
+    UIImage *gold=[UIImage imageNamed:@"redhat_games.png"];
+    UIImage *santa=[UIImage imageNamed:@"sleeping_santaclaus.png"];
+    UIImage *tsitter=[UIImage imageNamed:@"tsitter_square_sleeping.png"];
+    UIImage *monkey=[UIImage imageNamed:@"face_monkey.png"];
+    
+
+    
+    for(int i=1;i<=3;i++)
+    {
+        UIImageView *happyview=[[UIImageView alloc] initWithImage:happy];
+        UIImageView *goldview=[[UIImageView alloc] initWithImage:gold];
+        UIImageView *santaView=[[UIImageView alloc] initWithImage:santa];
+        UIImageView *tsitterview=[[UIImageView alloc] initWithImage:tsitter];
+        UIImageView *monkeyview=[[UIImageView alloc] initWithImage:monkey];
+
+        
+        NSArray *imgViewArray=[[NSArray alloc] initWithObjects:happyview,goldview,santaView,tsitterview,monkeyview, nil];
+        
+        
+        //NSLog(@"imgViewArray:%@",imgViewArray);
+        
+        NSString *columnname=[[NSString alloc] initWithFormat:@"column%d",i];
+        [self setValue:imgViewArray forKey:columnname];
+    }
+    srandom(time(NULL));
 }
 
 
@@ -100,6 +138,119 @@
 }
 
 
+-(IBAction)spin
+{
+    
+    times=[NSTimer scheduledTimerWithTimeInterval:0.05 target:self selector:@selector(function) userInfo:nil repeats:YES];
+    
+    
+    
+}
+
+
+-(void)function
+{
+    j++;
+    if(j%100==0)
+    {
+        [times invalidate];
+        times=nil;
+        int firstvalue=random()%1000;
+        [self.picker selectRow:firstvalue inComponent:0 animated:YES];       //得出结果
+        [self.picker reloadComponent:0];
+        int secondvalue=random()%1000;
+        [self.picker selectRow:secondvalue inComponent:1 animated:YES];
+        [self.picker reloadComponent:1];
+        int thirdvalue=random()%1000;
+        [self.picker selectRow:thirdvalue inComponent:2 animated:YES];
+        [self.picker reloadComponent:2];
+        NSLog(@"%d",firstvalue%5);
+        if (firstvalue%5==secondvalue%5&&firstvalue%5==thirdvalue%5) 
+        {
+            NSLog(@"3个一样");
+        }
+        else if(firstvalue%5==secondvalue%5||firstvalue%5==thirdvalue%5||secondvalue%5==thirdvalue%5)
+        {
+            NSLog(@"2个一样");
+        }
+        /*
+         for (int i=0; i<3; i++) {
+         int newValue=random()%1000;
+         if (newValue%5==lastval%5) {
+         numInRow++;
+         }
+         else numInRow=1;
+         lastval=newValue;
+         [picker selectRow:newValue inComponent:i animated:YES];
+         [picker reloadComponent:i];
+         if (numInRow==2) {
+         money=money+2*zhu;
+         }
+         else if(numInRow==3)
+         {
+         money=money+20*zhu;
+         }
+         }
+         
+         if (win) {
+         [display setText:@"win!"];
+         }
+         else [display setText:@"lose!"];
+         NSLog(@"%d",j);
+         */
+        j=0;
+        self.goButoon.enabled=YES;
+        
+    }
+    else
+        {for (int i=0; i<3; i++) {
+            [self.picker selectRow:j inComponent:i animated:YES];
+            [self.picker reloadComponent:i];
+            self.goButoon.enabled=NO;
+        }
+    }
+}
+
+
+
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+-(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 3;
+}
+-(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    return 1000;
+}
+-(UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view
+{
+    
+    NSString *arrayName=[[NSString alloc] initWithFormat:@"column%d",component+1];
+    NSArray *array=[self valueForKey:arrayName];
+    return [array objectAtIndex:row%5];
+    
+}
+-(NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    NSMutableArray *strings = [NSMutableArray arrayWithCapacity:5];
+    [strings addObject:@"Zero"];
+    [strings addObject:@"One"];
+    [strings addObject:@"Two"];
+    [strings addObject:@"Three"];
+    [strings addObject:@"Four"];
+    return [strings objectAtIndex:(row%5)];
+    
+}
+-(CGFloat)pickerView:(UIPickerView *)pickerView rowHeightForComponent:(NSInteger)component
+{
+    //return 128;
+    return 50;
+}
 
 - (void)didReceiveMemoryWarning
 {
